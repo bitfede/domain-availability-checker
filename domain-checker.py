ad7fe67f602f5c2639ea5013dc87f885a688413a
Fede Defaveri <f.g.defaveri@gmail.com>
2018-12-13 02:21:41 +0100

    Loops thru words of the dict and checks first line of whois record

--- domain-checker.py
+++ domain-checker.py
@@ -1,3 +1,25 @@
 #!/usr/local/bin/python3
 
+##
+# AUTHOR: Federico D.
+# DATE: December 2018
+# PURPOSE: Domain availability checker
+##
+
+#dependencies
 import requests
+import subprocess
+
+#open the dictionary
+dictionary = open('google-10000-english.txt', 'r')
+
+count = 0
+#loop thru every word in the dictionary
+for word in dictionary:
+    word = word.rstrip()
+    count = count + 1
+    whoisresult = subprocess.check_output('whois {}.com'.format(word), shell=True)
+    whoisresult = whoisresult.decode('ascii').split('\n')
+    print(whoisresult[0].strip('   '))
+    if (count == 50):
+        exit(0)
