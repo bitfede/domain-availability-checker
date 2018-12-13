be9c0c09a367059804fee0ca8c9ce098ba875446
Fede Defaveri <f.g.defaveri@gmail.com>
2018-12-13 02:44:14 +0100

    Writes looping logic and if statement for determining if domain is free or not

--- domain-checker.py
+++ domain-checker.py
@@ -12,14 +12,23 @@ import subprocess
 
 #open the dictionary
 dictionary = open('google-10000-english.txt', 'r')
+resultfile = open('available-domains.txt', 'a')
 
 count = 0
 #loop thru every word in the dictionary
 for word in dictionary:
-    word = word.rstrip()
     count = count + 1
-    whoisresult = subprocess.check_output('whois {}.com'.format(word), shell=True)
+    word = word.rstrip()
+    domain = '{}.com'.format(word)
+    whoisresult = subprocess.check_output('whois {}'.format(domain), shell=True)
     whoisresult = whoisresult.decode('ascii').split('\n')
-    print(whoisresult[0].strip('   '))
-    if (count == 50):
-        exit(0)
+    domainstatus = whoisresult[0].strip('   ')
+    if "No match for" in domainstatus:
+        #BINGO!
+        print("[!!!] DOMAIN AVAILABLE: {}".format(domain))
+        resultfile.write(domain)
+    else:
+        #do nothing
+        print("[*] DOMAIN {} NOT AVAILABLE".format(domain))
+
+print("Finish! Total Domains Scanned: {}".format(count))
